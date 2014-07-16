1b074fd55a4c74663ad72dce88189983f08bb451
Barry Ceelen <b@rryceelen.com>
2014-07-16 23:19:45 +0200

    asdf

--- Gruntfile.js
+++ Gruntfile.js
@@ -152,5 +152,5 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-imagemin');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
-	grunt.registerTask('default', ['sass:dist','autoprefixer','clean','copy:dist','imagemin']);
+	grunt.registerTask('default', ['sass:dist','autoprefixer','jade','clean','copy','imagemin']);
 };
